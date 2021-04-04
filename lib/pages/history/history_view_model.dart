18d07b61889b37d511adf1193bb76e10aedc236a
Oğuzhan Sevim <oguzhansevim@outlook.com>
2021-04-04 19:39:32 +0300

    return type added for function

--- lib/pages/history/history_view_model.dart
+++ lib/pages/history/history_view_model.dart
@@ -2,7 +2,7 @@ import 'package:qrcookie_v2/core/base/base_stateful.dart';
 import 'history.dart';
 
 abstract class HistoryViewModel extends BaseStateful<History> {
-  deleteBarcode(id) async {
+  void deleteBarcode(id) async {
     await databaseHelper.delete(id);
   }
 }
